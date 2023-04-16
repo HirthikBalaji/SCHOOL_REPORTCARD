a30ef8b46e6700533710ce96e2b924a17a6df9ff
Hirthik Balaji C <89502294+HirthikBalaji@users.noreply.github.com>
2023-04-16 21:36:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,16 @@
-import os
-import helper as s
 import glob
-def preprocess(input_file)
-    s.extract_pages(input_file)
-    direct = glob.glob("*.pdf")
-    for i in direct:
-        s.add_class_teacher_sign(i)
-        s.add_principal_sign(i)
-    all = glob.glob("*.pdf")
-    want = glob.glob("*_f*")
-    for i in want:
-        all.remove(i)
-    for j in all:
-        os.remove(j)
-    for k in want:
-        s.encrypt(k)
-    all = glob.glob("*.pdf")
-    want = glob.glob("*_E*")
-    for l in want:
-        all.remove(l)
-    for t in all:
-        os.remove(t)
+from whatsapp_document_sender import send
+from helper import add_signature, encrypt, extract_pages,read
 
+
+extract_pages("REPORTCARD.pdf")
+list1 = glob.glob("EXTRACT/*.pdf")
+for i in list1:
+    add_signature(i, principal_image_path='signature.jpg', ct_image_path="signature.jpg")
+list2 = glob.glob("EXTRACT/*.pdf")
+for i in list2:
+    encrypt(i)
+for i in read("REPORT.csv"):
+    message = f"Dear Parent,\n greetings from Adhyapana, hereby we have attached the Report card of your " \
+              f"ward\nPassword is:{i['admno']}"
+    send(i["phno"],f"EXTRACT/{i['admno']}.pdf",message=message)
